879f8400428a472649b14cf3f73a9e73ced14e69
gerizzo <gerardorizzo99@gmail.com>
2022-02-10 05:41:36 -0300

    SE AÑADIÓ UN FORMULARIO DE PAGO EN LA SECCIÓN DEL CARRITO

--- js/script.js
+++ js/script.js
@@ -388,6 +388,7 @@ let precioTotal = () => {
         totalPrice += element.precio * element.stock;
     });
     document.querySelector("#subtotal").textContent = totalPrice;
+    return totalPrice
 };
 
 let actualizarContadorCarrito = () => {
@@ -399,88 +400,146 @@ let actualizarContadorCarrito = () => {
 document.querySelector("#precioTotalCarritoIndex").addEventListener("click", () => {
     let cantidadCompra = 0
     for (let i = 0; i < carrito.length; i++) {
-        cantidadCompra += carrito[i].stock
+        cantidadCompra = cantidadCompra + carrito[i].stock
     }
-
     if (cantidadCompra == 0) {
+
         document.querySelector(".modal-header").innerHTML =
             `
-            <h5>Carrito Vacío<i class="fas fa-times-circle" style="margin-left: 4px;"></i></h5>
-        `
+                    <h5 class"mensajeCarritoVacio">Carrito Vacío<i class="fas fa-times-circle" style="margin-left: 4px;"></i></h5>
+                    <button type="button" style="float: right" class="btn-close" data-bs-dismiss="modal"></button>
+                `
         document.querySelector(".productosConfirmarCompra").innerHTML =
             `
-            <p>TU CARRITO SE ENCUENTRA VACÍO <i class="far fa-frown-open" style="margin-left: 1px"></i></p>
-        `
-    } else if (cantidadCompra == 1) {
-        document.querySelector(".modal-header").innerHTML =
-            `
-            <h5>Compra Realizada<i class="fas fa-check" style="margin-left: 4px;"></i></h5>
-        `
-        document.querySelector(".productosConfirmarCompra").innerHTML =
-            `
-            <p>GRACIAS POR COMPRAR ${cantidadCompra} DISCO EN <strong>VINILOS FANKY</strong></p>
-        `
-        /* REDUCIR STOCK */
-
-        carrito.forEach(disco => {
-            let discoStock = todosLosDiscos.filter(x => x.nombreDisco == disco.nombreDisco);
-            let numeroARestar = parseFloat(disco.stock);
-            discoStock[0].comprar(numeroARestar);
-        });
-
-        /* REDUCIR STOCK */
-
-        carrito = []
-        document.querySelector("#contadorProductosCarrito").textContent = carrito.length;
-        document.querySelector("#subtotal").textContent = 0;
-        let hijosCarrito = contenedorProductosCarrito.childNodes
-        while (hijosCarrito.length != 0) {
-            hijosCarrito[0].remove();
-        }
+                    <p class"mensajeCarritoVacio">TU CARRITO SE ENCUENTRA VACÍO <i class="far fa-frown-open" style="margin-left: 1px"></i></p>
+          `
     } else {
+
+        let datosUsuario = {
+            nombreUsuario: undefined,
+            apellidoUsuario: undefined,
+            mailUsuario: undefined,
+            telefonoUsuario: undefined,
+        }
+
         document.querySelector(".modal-header").innerHTML =
             `
-            <h5>Compra Realizada<i class="fas fa-check" style="margin-left: 4px;"></i></h5>
-        `
-        document.querySelector(".productosConfirmarCompra").innerHTML =
-            `
-            <p>GRACIAS POR COMPRAR ${cantidadCompra} DISCOS EN <strong>VINILOS FANKY</strong></p>
-        `
+                <h5>CHECKOUT <i class="fas fa-cash-register" style="margin-left: 4px;"></i></h5>
+                <button type="button" style="float: right" class="btn-close" data-bs-dismiss="modal"></button>
+                 `
 
-        /* REDUCIR STOCK */
+        document.querySelector(".modal-body").innerHTML = `
+                    <form id="formularioIndex">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-6">
+                                    <div class="mb-3">
+                                        <label for="name" class="form-label">Nombre </label>
+                                        <input type="text" required name="nombre" id="name" class="form-control">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="surname" class="form-label">Apellido </label>
+                                        <input type="text" required name="apellido" id="surname" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="col-6">
+                                    <div class="mb-3">
+                                        <label for="mail" class="form-label">Email </label>
+                                        <input type="email" required name="email" id="mail" class="form-control">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="phone" class="form-label">Teléfono </label>
+                                        <input type="tel" required name="phone" id="phone" class="form-control">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-1">
+                                    <span></span>
+                                </div>
+                                <div class="col-10">
+                                    <label for="metodoPago" class="form-label"><i class="fa fa-money-check" style="margin-right: 5px"></i>Método de Pago</label>
+                                    <br>
+                                    <input type="radio" name="metodoPago" value="tarjeta" style="margin-right: 5px" required>Tarjeta de Crédito/Débito
+                                    <br>
+                                    <input type="radio" name="metodoPago" value="transferencia" style="margin-right: 5px" required>Transferencia Bancaria &nbsp;  &nbsp;
+                                </div>
 
-        carrito.forEach(disco => {
-            let discoStock = todosLosDiscos.filter(x => x.nombreDisco == disco.nombreDisco);
-            let numeroARestar = parseFloat(disco.stock);
-            discoStock[0].comprar(numeroARestar);
-        });
+                                <div class="col-1">
+                                    <span></span>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="mt-3 col-12 cargando">
+                                    <h4>Total $ ${precioTotal()} </h4>
+                                    <button type="submit" class="btn btn-outline-success mt-2" id="btnSubmit">PAGAR</button>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="mt-3 col-12 contenedorErrorFormulario">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                        `
 
-        /* REDUCIR STOCK */
+        let btnSubmit = document.querySelector("#btnSubmit");
+        btnSubmit.addEventListener("click", (event) => {
+            event.preventDefault()
+            datosUsuario.nombreUsuario = document.querySelector("#name").value
+            datosUsuario.apellidoUsuario = document.querySelector("#surname").value
+            datosUsuario.mailUsuario = document.querySelector("#mail").value;
+            datosUsuario.telefonoUsuario = document.querySelector("#phone").value;
+            if (datosUsuario.nombreUsuario.length >= 2 && datosUsuario.apellidoUsuario.length >= 2 && datosUsuario.mailUsuario.includes("@") && datosUsuario.mailUsuario.length > 4 && datosUsuario.telefonoUsuario.length >= 7) {
+                if (document.querySelector("#parrafoError") != undefined) {
+                    document.querySelector("#parrafoError").remove()
+                }
+                document.querySelector(".cargando").innerHTML = `
+                    <br>
+                    <br>
+                    <img src="img/cargando.gif" alt="cargando" style="width: 50px; height: 50px" >
+                `
+                setTimeout(function () {
+                    document.querySelector(".modal-header").innerHTML =
+                        `
+                    <h5>Compra Realizada<i class="fa fa-check" style="margin-left: 4px;"></i></h5>
+                    <button type="button" style="float: right" class="btn-close" data-bs-dismiss="modal"></button>
+                `
+                    document.querySelector(".modal-body").innerHTML =
+                        `
+                    <h3>¡Gracias ${datosUsuario.nombreUsuario} por tu compra!</h3>
+                    <br>
+                    <h4>Te hemos enviado tu factura electrónica a ${datosUsuario.mailUsuario}</h4>
+                    <br>
+                    <br>
+                    <button type="button" class="btn btn-success" data-bs-dismiss="modal">Aceptar<i
+                    class="fas fa-hand-spock" style="margin-left: 4px;"></i></button>
+                `
+                    /* COMPRAR Y REDUCIR STOCK */
 
-        carrito = []
-        document.querySelector("#contadorProductosCarrito").textContent = carrito.length;
-        document.querySelector("#subtotal").textContent = 0;
-        let hijosCarrito = contenedorProductosCarrito.childNodes
-        while (hijosCarrito.length != 0) {
-            hijosCarrito[0].remove();
-        }
+                    carrito.forEach(disco => {
+                        let discoStock = todosLosDiscos.filter(x => x.nombreDisco == disco.nombreDisco);
+                        let numeroARestar = parseFloat(disco.stock);
+                        discoStock[0].comprar(numeroARestar);
+                    })
+
+                    /* REDUCIR STOCK */
+
+                    carrito = []
+                    document.querySelector("#contadorProductosCarrito").textContent = carrito.length;
+                    document.querySelector("#subtotal").textContent = 0;
+                    let hijosCarrito = contenedorProductosCarrito.childNodes
+                    while (hijosCarrito.length != 0) {
+                        hijosCarrito[0].remove();
+                    }
+                }, 3000);
+            } else {
+                document.querySelector(".contenedorErrorFormulario").innerHTML = ` 
+                <p style="color: red; font-weight: bold" id="parrafoError">*Debes completar todos los campos*</p>
+                `
+            }
+
+        })
     }
-});
-
-/* MODO OSCURO */
-
-/* let botonModoOscuro = document.querySelector("#modoOscuro");
-botonModoOscuro.addEventListener("click", modoOscuro);
-
-function modoOscuro() {
-    document.querySelector("nav").classList.toggle("navbar-dark");
-    document.querySelector("nav").classList.toggle("bg-dark");
-    document.querySelector("#solLuna").classList.toggle("fa-sun");
-    document.querySelector("body").classList.toggle("bg-dark");
-    document.querySelector("#anchor").classList.toggle("anchorToggle");
-    let textosExh = document.querySelectorAll(".textoExhibidor");
-    for (let i = 0; i < textosExh.length; i++) {
-        textosExh[i].classList.toggle("textoExhibidorBlanco");
-    }
-}
- */
+})
